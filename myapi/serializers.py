1e0fbff8b8b6cabc9de7e5cca512de9cab0351ab
Sowmya2529 <32456629+Sowmya2529@users.noreply.github.com>
2020-07-03 00:45:49 +0530

    Update serializers.py

--- myapi/serializers.py
+++ myapi/serializers.py
@@ -2,19 +2,6 @@
 from .models import *
 from rest_framework import serializers, fields
 
-'''
-class BranchesSerializer(serializers.ModelSerializer):
-   class Meta:
-   		model = Branches
-   		fields = '__all__'
-
-
-class BanksSerializer(serializers.ModelSerializer):
-	branch_details = BranchesSerializer(read_only=True,many=True)
-	class Meta:
-		model = Banks
-		fields = '__all__'
-'''
 
 class BanksSerializer(serializers.ModelSerializer):
 	#branch_details = BranchesSerializer(read_only=True,many=True)
